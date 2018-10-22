001be2e20b36c2089a26f329e38f0b92f95d99d7
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-10-22 20:41:05 +0200

    feat: reduce like propose

--- src/index.js
+++ src/index.js
@@ -6,19 +6,23 @@ import "./styles.css";
 
 const myArr = ["a", "a", "b", "b", "c", "z", "a"];
 
-console.log(
-  myArr.reduce((acc, item) => {
-    let newItem = "";
+let test = myArr.reduce((acc, item) => {
+  let flag = "";
 
-    acc.find(itemAcc => {
-      return itemAcc === item;
-    })
-      ? (newItem = null)
-      : (newItem = item);
+  acc.find(accItem => {
+    if (accItem === item) {
+      return (flag = true);
+    } else {
+      return (flag = false);
+    }
+  });
 
-    return [...acc, newItem];
-  }, [])
-);
+  if (flag) {
+    return acc;
+  } else {
+    return [...acc, item];
+  }
+}, []);
 
 class App extends Component {
   state = {
@@ -27,40 +31,58 @@ class App extends Component {
     propose: [],
     secret: "abcd",
     letter: "",
-    active: true
+    counterClass: "active"
   };
 
   handleCounter = () => {
-    this.state.counter < 4
+    this.state.counter < 10
       ? this.setState(prevState => ({ counter: prevState.counter + 1 }))
-      : this.handleFini();
+      : this.setState(prevState => ({
+          ...prevState,
+          counterClass: "stopped",
+          counter: "lost"
+        }));
   };
 
-  handleFini = () => {
-    this.setState({ active: false });
+  reducePropose = valeur => {
+    this.setState(prevState => {
+      let flag = false;
+      prevState.propose.find(item => {
+        if (item === valeur) {
+          return (flag = true);
+        }
+      });
+      if (flag === false) {
+        return { propose: [...prevState.propose, valeur] };
+      }
+    });
   };
 
   handleLetter = e => {
     const valeur = e.target.value;
     this.setState({ letter: "" });
     this.handleCounter();
-    this.setState(prevState => ({ propose: [...prevState.propose, valeur] }));
-    console.log(this.state.propose);
+    this.reducePropose(valeur);
   };
 
-  //handleFini =()=>console.log("FINI")
-
   render() {
-    const { refus, secret, propose, letter, counter } = this.state;
+    const {
+      refus,
+      secret,
+      propose,
+      letter,
+      counter,
+      counterClass
+    } = this.state;
 
     return (
       <div>
         <div>{secret}</div>
-        <div>{counter}</div>
+        <div className={counterClass}>{counter}</div>
 
         <input type="text" onChange={this.handleLetter} value={letter} />
 
-        <div>refus : {propose.map(item => item)}</div>
+        <div>lettres proposées : {propose.map(item => item)}</div>
       </div>
     );
   }
